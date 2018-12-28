e6b533bfe664f8ed50784560609872f4093de9e3
Fabio Bertagna <flbertagna@gmail.com>
2018-12-28 23:39:46 +0100

    added startup script

---
+++ startup-script.sh
@@ -0,0 +1,30 @@
+#
+#       Title: startup-script .sh
+# Author: Fabio Bertagna
+# Date created:  Fri 28 Dec 2018 11:31:39 PM CET 
+# Comment: 
+
+ 
+
+#!/bin/bash
+
+cd
+wget -q --tries=10 --timeout=20 --spider http://google.com
+if [[ $? -eq 0 ]]; then
+        echo "Online"
+else
+        echo "Offline"
+        exit 1
+fi
+
+if [ -f ./hackathon-setup.sh ]; then
+   exit 1
+else
+    wget https://raw.githubusercontent.com/DonGiovanni83/raspberry-pi-setup/master/hackathon-setup.sh
+    ./hackathon-setup.sh
+fi
+
+
+
+#//startup-script .sh 
+
