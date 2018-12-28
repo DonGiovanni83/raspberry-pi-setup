18fd1be310d4df9ac8e56ee92b414039d3d93a5d
Fabio Bertagna <flbertagna@gmail.com>
2018-12-29 00:05:21 +0100

    .bashrc setup permission fix

--- hackathon-setup.sh
+++ hackathon-setup.sh
@@ -26,8 +26,8 @@ sudo echo "LC_ALL=en_US.UTF-8" >> sudo /etc/environment
 sudo echo "en_US.UTF-8 UTF-8" >> sudo /etc/locale.gen
 sudo echo "LANG=en_US.UTF-8" > sudo /etc/locale.conf
 sudo locale-gen en_US.UTF-8
-sudo echo "export LC_ALL=C" >> .bashrc
-sudo echo "alias ll='ls -la'" >> .bashrc
+sudo echo "export LC_ALL=C" >> sudo .bashrc
+sudo echo "alias ll='ls -la'" >> sudo .bashrc
 source ~/.bashrc
 sudo apt-get update
 sudo apt-get upgrade -y
